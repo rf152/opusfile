865dbf2db0b59e94db7ee9292c279329c7f50126
Richard Franks <git@rf152.co.uk>
2025-03-06 22:19:38 +0000

    Added opus_comment_header_test

---
+++ tests/opus_comment_header_test.cc
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2025 R Franks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+#include <gtest/gtest.h>
+#include <OpusCommentHeader.h>
+#include <OpusFileCommon.h>
+#include "data.h"
+
+// Demonstrate some basic assertions.
+TEST(OpusCommentHeader, BasicParsing) {
+  OpusCommentHeader_t header;
+  ASSERT_EQ(opus_comment_header_load(&header, validtestdata + 0x4B),
+            OPUSFILE_OK);
+}
+TEST(OpusCommentHeader, BasicParsingError) {
+  OpusCommentHeader_t header;
+  ASSERT_EQ(opus_comment_header_load(&header, invalidtestdata + 0x4B),
+            OPUSFILE_BAD_MAGIC);
+}
+
+TEST(OpusPage, ParsingHeader) {
+  OpusCommentHeader_t header;
+  opus_comment_header_load(&header, validtestdata + 0x4B);
+  EXPECT_EQ(header.signature, 0x736761547375704F);
+  EXPECT_EQ(header.vendor_string_length, 11);
+}
